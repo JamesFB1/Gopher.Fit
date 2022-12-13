f0cedadf8e1830a9f29c489cac52797a9d2c8214
James Fraser Barsby <jfraserbarsby@gmail.com>
2022-12-13 15:08:08 -0500

    style

--- R/bootstrap_SE_CI.R
+++ R/bootstrap_SE_CI.R
@@ -1,18 +1,18 @@
-SE_CI <- function(my_data = tortoise){
+SE_CI = function(my_data = tortoise){
 
-  my_data <- my_data %>%
-  rowid_to_column("ID") %>%  # add ID column
-  select(-type, -density)    # delete type and density columns
+  my_data = my_data %>%
+    rowid_to_column("ID") %>%  # add ID column
+    select(-type, -density)    # delete type and density columns
 
-  fit <- run_model(my_data, example = "tortoise")  # fit the model
+  fit = run_model(my_data, example = "tortoise")  # fit the model
 
-  my_data <- my_data %>%
+  my_data = my_data %>%
     mutate(.fitted = fit$beta[[2]] * prev)  # make a column for beta_1 * prev
 
   ## Generate bootstrap samples
   B = 1000
 
-  bootstrap <- tibble(B = 1:B) %>%
+  bootstrap = tibble(B = 1:B) %>%
     crossing(my_data) %>%
     mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
            eta = fit$beta[[1]] + log(Area) + .fitted + z,  # eta_ij* = beta_0 + beta_1xij1 + log(xi2) + zi*
@@ -27,19 +27,19 @@ SE_CI <- function(my_data = tortoise){
     # ungroup()
 
   ## Compute bootstrap standard errors and confidence intervals for beta_0, beta_1 and sigma_sqr
-  Intercept <- bootstrap %>%
+  Intercept = bootstrap %>%
     filter(term == "(Intercept)") %>%  # filter for the Intercept term (beta_0)
     summarize(SE = sd(estimate),  # estimate column has parameter estimate for Intercept term
               Lower95 = quantile(estimate, 0.025),
               Upper95 = quantile(estimate, 0.975))
 
-  Prev <- bootstrap %>%
+  Prev = bootstrap %>%
     filter(term == "prev") %>%  # do the same for beta_1
     summarize(SE = sd(estimate),
               Lower95 = quantile(estimate, 0.025),
               Upper95 = quantile(estimate, 0.975))
 
-  Sigmasq <- bootstrap %>%
+  Sigmasq = bootstrap %>%
     filter(term == "sd__(Intercept)") %>%  # not quite sure about this, but checked estimation_functions.R line 103 to try and extract sigmasq from output
     mutate(estimate = ifelse(estimate == 0, 0.100, estimate)) %>%  # followed what was done in estimation_functions.R - if sigmasq == 0, set it to 0.1
     summarize(SE = sd(estimate),
