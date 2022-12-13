18891b5af27fba2d955890f6b9bf5f75108de872
Dora Cao <doracao83@gmail.com>
2022-12-13 11:31:01 -0500

    minor changes and added comments

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,41 +1,45 @@
-bootsrap = function(my_data = tortoise){
-my_data = my_data %>%
-  rowid_to_column("ID") %>%
-  select(-type, -density)
+bootstrap <- function(my_data = tortoise){
 
-fit = run_model(my_data, example = "tortoise")
+  my_data <- my_data %>%
+  rowid_to_column("ID") %>%  # add ID column
+  select(-type, -density)    # delete type and density columns
 
-my_data = my_data %>%
-  mutate(.fitted = fit$beta[[2]] * prev)
+  fit <- run_model(my_data, example = "tortoise")  # fit the model
 
-B = 1000
+  my_data <- my_data %>%
+    mutate(.fitted = fit$beta[[2]] * prev)  # make a column for beta_1 * prev
 
-bootstrap = tibble(B = 1:B) %>%
-  crossing(my_data) %>%
-  mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
-         eta = fit$beta[[1]] + log(Area) + .fitted + z,
-         shells = rpois(n(), lambda = exp(eta))) %>%
-  nest_by(B) %>%
-  summarize(Intercept = run_model(data = data,example = "tortoise")$beta[[1]],
-            Prev = run_model(data = data,example = "tortoise")$beta[[2]],
-            Sigmasq = run_model(data = data,example = "tortoise")$sigmasq) %>%
-  ungroup()
+  ## Generate bootstrap samples: Resample Residuals
+  B = 1000
 
-Intercept = bootstrap %>%
-            summarize(SE = sd(Intercept),
-            Lower95 = quantile(Intercept, 0.025),
-            Upper95 = quantile(Intercept, 0.975))
+  bootstrap <- tibble(B = 1:B) %>%
+    crossing(my_data) %>%
+    mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
+           eta = fit$beta[[1]] + log(Area) + .fitted + z,  # eta_ij* = beta_0 + beta_1xij1 + log(xi2) + zi*
+           shells = rpois(n(), lambda = exp(eta))) %>%  # Yij*~Poisson(lambda) where lambda = mu_ij* = exp(eta_ij*)
+    nest_by(B) %>%
+    summarize(Intercept = run_model(data = data,example = "tortoise")$beta[[1]],  # refit the model, bootstrapped estimate of beta_0
+              Prev = run_model(data = data,example = "tortoise")$beta[[2]],       # bootstrapped estimate of beta_1
+              Sigmasq = run_model(data = data,example = "tortoise")$sigmasq) %>%  # bootstrapped estimate of sigma_sqr
+    ungroup()
 
-Prev = bootstrap %>%
-            summarize(SE = sd(Prev),
-            Lower95 = quantile(Prev, 0.025),
-            Upper95 = quantile(Prev, 0.975))
+  ## Compute bootstrap standard errors and confidence intervals for beta_0, beta_1 and sigma_sqr
+  Intercept <- bootstrap %>%
+              summarize(SE = sd(Intercept),
+              Lower95 = quantile(Intercept, 0.025),
+              Upper95 = quantile(Intercept, 0.975))
 
-Sigmasq = bootstrap %>%
-            summarize(SE = sd(Sigmasq),
-            Lower95 = quantile(Sigmasq, 0.025),
-            Upper95 = quantile(Sigmasq, 0.975))
-return(list(Intercept = Intercept,
-            Prev = Prev,
-            Sigmasq = Sigmasq))
+  Prev <- bootstrap %>%
+              summarize(SE = sd(Prev),
+              Lower95 = quantile(Prev, 0.025),
+              Upper95 = quantile(Prev, 0.975))
+
+  Sigmasq <- bootstrap %>%
+              summarize(SE = sd(Sigmasq),
+              Lower95 = quantile(Sigmasq, 0.025),
+              Upper95 = quantile(Sigmasq, 0.975))
+
+  return(list(Intercept = Intercept,
+              Prev = Prev,
+              Sigmasq = Sigmasq))
 }
