98fec6012513e347be84e50e283f751a99f1f5cb
James Fraser Barsby <jfraserbarsby@gmail.com>
2022-12-12 19:37:19 -0500

    Added some package dependencies

---
+++ R/Gopher.Fit-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr %>%
+#' @importFrom dplyr tibble
+#' @importFrom lme4 glmer
+#' @importFrom lme4 ranef
+## usethis namespace: end
+NULL
