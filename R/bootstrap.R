c1a507ef1b490425e4fd90f5b3c0fd0904fbac79
James Fraser Barsby <jfraserbarsby@gmail.com>
2022-12-12 22:14:00 -0500

    Updated bootstrap function for SE and CI

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,19 +1,41 @@
-tortoise = tortoise %>%
+bootsrap = function(my_data = tortoise){
+my_data = my_data %>%
   rowid_to_column("ID") %>%
   select(-type, -density)
 
-fit = run_model(tortoise, example = "tortoise")
+fit = run_model(my_data, example = "tortoise")
 
-tortoise = tortoise %>%
+my_data = my_data %>%
   mutate(.fitted = fit$beta[[2]] * prev)
 
-B = 10000
-n = nrow(tortoise)
+B = 1000
+
 bootstrap = tibble(B = 1:B) %>%
-  crossing(tortoise) %>%
+  crossing(my_data) %>%
   mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
          eta = fit$beta[[1]] + log(Area) + .fitted + z,
          shells = rpois(n(), lambda = exp(eta))) %>%
-  nest_by(B)
+  nest_by(B) %>%
+  summarize(Intercept = run_model(data = data,example = "tortoise")$beta[[1]],
+            Prev = run_model(data = data,example = "tortoise")$beta[[2]],
+            Sigmasq = run_model(data = data,example = "tortoise")$sigmasq) %>%
+  ungroup()
 
+Intercept = bootstrap %>%
+            summarize(SE = sd(Intercept),
+            Lower95 = quantile(Intercept, 0.025),
+            Upper95 = quantile(Intercept, 0.975))
 
+Prev = bootstrap %>%
+            summarize(SE = sd(Prev),
+            Lower95 = quantile(Prev, 0.025),
+            Upper95 = quantile(Prev, 0.975))
+
+Sigmasq = bootstrap %>%
+            summarize(SE = sd(Sigmasq),
+            Lower95 = quantile(Sigmasq, 0.025),
+            Upper95 = quantile(Sigmasq, 0.975))
+return(list(Intercept = Intercept,
+            Prev = Prev,
+            Sigmasq = Sigmasq))
+}
