fb0a27ebe3bd222a7f361349e12bf0a1e86e7d28
James Fraser Barsby <jfraserbarsby@gmail.com>
2022-12-13 18:12:15 -0500

    Edited function

--- R/bootstrap_SE_CI.R
+++ R/bootstrap_SE_CI.R
@@ -1,15 +1,30 @@
+#' Final Project SE and CI
+#'
+#' @param my_data the data set you're using
+#'
+#' @return a list of the SE and CI of each estimate
+#' @export
+#'
+#' @examples
+#' # load tortoise data
+#' load("~/Gopher.Fit/data/tortoise.rda")
+#'
+#' # compute SE and CI
+#' SE_CI(tortoise)
 SE_CI = function(my_data = tortoise){
 
   my_data = my_data %>%
     rowid_to_column("ID") %>%  # add ID column
     select(-type, -density)    # delete type and density columns
 
-  fit = run_model(my_data, example = "tortoise")  # fit the model
+  # fit the model
+  fit = run_model(my_data, example = "tortoise")
 
+  # make a column for beta_1 * prev
   my_data = my_data %>%
-    mutate(.fitted = fit$beta[[2]] * prev)  # make a column for beta_1 * prev
+    mutate(.fitted = fit$beta[[2]] * prev)
 
-  ## Generate bootstrap samples
+  # generate bootstrap samples
   B = 1000
 
   bootstrap = tibble(B = 1:B) %>%
@@ -18,50 +33,43 @@ SE_CI = function(my_data = tortoise){
            eta = fit$beta[[1]] + log(Area) + .fitted + z,  # eta_ij* = beta_0 + beta_1xij1 + log(xi2) + zi*
            shells = rpois(n(), lambda = exp(eta))) %>%  # Yij*~Poisson(lambda) where lambda = mu_ij* = exp(eta_ij*)
     nest_by(B) %>%
-    summarize(tidy(suppressMessages(glmer(shells ~ prev + offset(log(Area)) + factor(year) + (1|Site),
-                                          family = poisson, data = data))), .groups = "drop")  # fit the model the way Prof. Bonner did
+    summarize(values = suppressMessages(run_model(data = data)), .groups = "drop")
 
-    # summarize(Intercept = run_model(data = data,example = "tortoise")$beta[[1]],  # refit the model, bootstrapped estimate of beta_0
-    #           Prev = run_model(data = data,example = "tortoise")$beta[[2]],       # bootstrapped estimate of beta_1
-    #           Sigmasq = run_model(data = data,example = "tortoise")$sigmasq) %>%  # bootstrapped estimate of sigma_sqr
-    # ungroup()
-
-  ## Compute bootstrap standard errors and confidence intervals for beta_0, beta_1 and sigma_sqr
+  # extract bootstrapped estimates
   Intercept = bootstrap %>%
-    filter(term == "(Intercept)") %>%  # filter for the Intercept term (beta_0)
-    summarize(SE = sd(estimate),  # estimate column has parameter estimate for Intercept term
-              Lower95 = quantile(estimate, 0.025),
-              Upper95 = quantile(estimate, 0.975))
+    select(values) %>%
+    filter(row_number() %% 4 == 1) %>%
+    unnest(cols = values) %>%
+    filter(row_number() %% 4 == 1)
 
   Prev = bootstrap %>%
-    filter(term == "prev") %>%  # do the same for beta_1
-    summarize(SE = sd(estimate),
-              Lower95 = quantile(estimate, 0.025),
-              Upper95 = quantile(estimate, 0.975))
+    select(values) %>%
+    filter(row_number() %% 4 == 1) %>%
+    unnest(cols = values) %>%
+    filter(row_number() %% 4 == 2)
 
   Sigmasq = bootstrap %>%
-    filter(term == "sd__(Intercept)") %>%  # not quite sure about this, but checked estimation_functions.R line 103 to try and extract sigmasq from output
-    mutate(estimate = ifelse(estimate == 0, 0.100, estimate)) %>%  # followed what was done in estimation_functions.R - if sigmasq == 0, set it to 0.1
-    summarize(SE = sd(estimate),
-              Lower95 = quantile(estimate, 0.025),
-              Upper95 = quantile(estimate, 0.975))
+    select(values) %>%
+    filter(row_number() %% 4 == 2) %>%
+    unnest(cols = values)
 
+  # compute standard errors and confidence intervals
+  Intercept = Intercept %>%
+    summarize(SE = sd(values),
+              Lower95 = quantile(values, 0.025),
+              Upper95 = quantile(values, 0.975))
 
-  # Intercept <- bootstrap %>%
-  #             summarize(SE = sd(Intercept),
-  #             Lower95 = quantile(Intercept, 0.025),
-  #             Upper95 = quantile(Intercept, 0.975))
-  #
-  # Prev <- bootstrap %>%
-  #             summarize(SE = sd(Prev),
-  #             Lower95 = quantile(Prev, 0.025),
-  #             Upper95 = quantile(Prev, 0.975))
-  #
-  # Sigmasq <- bootstrap %>%
-  #             summarize(SE = sd(Sigmasq),
-  #             Lower95 = quantile(Sigmasq, 0.025),
-  #             Upper95 = quantile(Sigmasq, 0.975))
+  Prev = Prev %>%
+    summarize(SE = sd(values),
+              Lower95 = quantile(values, 0.025),
+              Upper95 = quantile(values, 0.975))
 
+  Sigmasq = Sigmasq %>%
+    summarize(SE = sd(values),
+              Lower95 = quantile(values, 0.025),
+              Upper95 = quantile(values, 0.975))
+
+  # return values
   return(list(Intercept = Intercept,
               Prev = Prev,
               Sigmasq = Sigmasq))
