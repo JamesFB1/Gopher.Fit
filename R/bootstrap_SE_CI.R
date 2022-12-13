024aa0606daedccbf69b07a20cecee7250934c7e
James Fraser Barsby <jfraserbarsby@gmail.com>
2022-12-13 13:05:40 -0500

    Added Roxygen Skeleton

--- R/bootstrap_SE_CI.R
+++ R/bootstrap_SE_CI.R
@@ -1,41 +1,62 @@
-bootsrap = function(my_data = tortoise){
-my_data = my_data %>%
-  rowid_to_column("ID") %>%
-  select(-type, -density)
+#' Final Project Bootstrap SE and CI
+#'
+#' @param my_data data set
+#'
+#' @return A list with bootstrap standard errors and confidence intervals for each estimate
+#' @export
+#'
+#' @examples
+#' ## Load Data
+#' load("~/Gopher.Fit/data/tortoise.rda")
+#'
+#' ## Compute Bootstrap
+#' SE_CI(tortoise)
+SE_CI = function(my_data = tortoise){
 
-fit = run_model(my_data, example = "tortoise")
+  # Clean up the data, and add ID
+  my_data = my_data %>%
+    rowid_to_column("ID") %>%
+    select(-type, -density)
 
-my_data = my_data %>%
-  mutate(.fitted = fit$beta[[2]] * prev)
+  # Fit GLMM
+  fit = run_model(my_data, example = "tortoise")
 
-B = 1000
+  # Add fitted data to data
+  my_data = my_data %>%
+    mutate(.fitted = fit$beta[[2]] * prev)
 
-bootstrap = tibble(B = 1:B) %>%
-  crossing(my_data) %>%
-  mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
-         eta = fit$beta[[1]] + log(Area) + .fitted + z,
-         shells = rpois(n(), lambda = exp(eta))) %>%
-  nest_by(B) %>%
-  summarize(Intercept = run_model(data = data,example = "tortoise")$beta[[1]],
-            Prev = run_model(data = data,example = "tortoise")$beta[[2]],
-            Sigmasq = run_model(data = data,example = "tortoise")$sigmasq) %>%
-  ungroup()
+  # Generate bootstrap samples
+  B = 1000
 
-Intercept = bootstrap %>%
-            summarize(SE = sd(Intercept),
-            Lower95 = quantile(Intercept, 0.025),
-            Upper95 = quantile(Intercept, 0.975))
+  bootstrap = tibble(B = 1:B) %>%
+    crossing(my_data) %>%
+    mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
+           eta = fit$beta[[1]] + log(Area) + .fitted + z,
+           shells = rpois(n(), lambda = exp(eta))) %>%
+    nest_by(B) %>%
+    summarize(Intercept = run_model(data = data,example = "tortoise")$beta[[1]],
+              Prev = run_model(data = data,example = "tortoise")$beta[[2]],
+              Sigmasq = run_model(data = data,example = "tortoise")$sigmasq) %>%
+    ungroup()
 
-Prev = bootstrap %>%
-            summarize(SE = sd(Prev),
-            Lower95 = quantile(Prev, 0.025),
-            Upper95 = quantile(Prev, 0.975))
+  # Compute bootstrap standard error and confidence interval for each estimate
+  Intercept = bootstrap %>%
+    summarize(SE = sd(Intercept),
+              Lower95 = quantile(Intercept, 0.025),
+              Upper95 = quantile(Intercept, 0.975))
 
-Sigmasq = bootstrap %>%
-            summarize(SE = sd(Sigmasq),
-            Lower95 = quantile(Sigmasq, 0.025),
-            Upper95 = quantile(Sigmasq, 0.975))
-return(list(Intercept = Intercept,
-            Prev = Prev,
-            Sigmasq = Sigmasq))
+  Prev = bootstrap %>%
+    summarize(SE = sd(Prev),
+              Lower95 = quantile(Prev, 0.025),
+              Upper95 = quantile(Prev, 0.975))
+
+  Sigmasq = bootstrap %>%
+    summarize(SE = sd(Sigmasq),
+              Lower95 = quantile(Sigmasq, 0.025),
+              Upper95 = quantile(Sigmasq, 0.975))
+
+  # return value
+  return(list(Intercept = Intercept,
+              Prev = Prev,
+              Sigmasq = Sigmasq))
 }
