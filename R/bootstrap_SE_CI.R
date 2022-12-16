f16316330729fac883bc14af53f6ac23904a4ec3
Dora Cao <doracao83@gmail.com>
2022-12-15 21:56:59 -0500

    added some comments

--- R/bootstrap_SE_CI.R
+++ R/bootstrap_SE_CI.R
@@ -13,21 +13,21 @@
 #' SE_CI(tortoise)
 SE_CI = function(my_data = tortoise){
 
-  my_data = my_data %>%
+  my_data <- my_data %>%
     rowid_to_column("ID") %>%  # add ID column
     select(-type, -density)    # delete type and density columns
 
   # fit the model
-  fit = run_model(my_data, example = "tortoise")
+  fit <- run_model(my_data, example = "tortoise")
 
   # make a column for beta_1 * prev
-  my_data = my_data %>%
+  my_data <- my_data %>%
     mutate(.fitted = fit$beta[[2]] * prev)
 
   # generate bootstrap samples
   B = 1000
 
-  bootstrap = tibble(B = 1:B) %>%
+  bootstrap <- tibble(B = 1:B) %>%
     crossing(my_data) %>%
     mutate(z = rep(rnorm(n()/3, mean = 0, sd = sqrt(fit$sigmasq)), each = 3),
            eta = fit$beta[[1]] + log(Area) + .fitted + z,  # eta_ij* = beta_0 + beta_1xij1 + log(xi2) + zi*
@@ -36,35 +36,35 @@ SE_CI = function(my_data = tortoise){
     summarize(values = suppressMessages(run_model(data = data)), .groups = "drop")
 
   # extract bootstrapped estimates
-  Intercept = bootstrap %>%
+  Intercept <- bootstrap %>%
     select(values) %>%
-    filter(row_number() %% 4 == 1) %>%
+    filter(row_number() %% 4 == 1) %>%  # run_model has a list of 4 outputs, 1 to take the beta values
     unnest(cols = values) %>%
-    filter(row_number() %% 4 == 1)
+    filter(row_number() %% 4 == 1)  # 1 to take the estimate for the Intercept
 
-  Prev = bootstrap %>%
+  Prev <- bootstrap %>%
     select(values) %>%
-    filter(row_number() %% 4 == 1) %>%
+    filter(row_number() %% 4 == 1) %>%  # run_model has a list of 4 outputs, 1 to take the beta values
     unnest(cols = values) %>%
-    filter(row_number() %% 4 == 2)
+    filter(row_number() %% 4 == 2)  # 2 to take the estimate for prev
 
-  Sigmasq = bootstrap %>%
+  Sigmasq <- bootstrap %>%
     select(values) %>%
-    filter(row_number() %% 4 == 2) %>%
+    filter(row_number() %% 4 == 2) %>%  # run_model has a list of 4 outputs, 2 to take the sigmasq value
     unnest(cols = values)
 
   # compute standard errors and confidence intervals
-  Intercept = Intercept %>%
+  Intercept <- Intercept %>%
     summarize(SE = sd(values),
               Lower95 = quantile(values, 0.025),
               Upper95 = quantile(values, 0.975))
 
-  Prev = Prev %>%
+  Prev <- Prev %>%
     summarize(SE = sd(values),
               Lower95 = quantile(values, 0.025),
               Upper95 = quantile(values, 0.975))
 
-  Sigmasq = Sigmasq %>%
+  Sigmasq <- Sigmasq %>%
     summarize(SE = sd(values),
               Lower95 = quantile(values, 0.025),
               Upper95 = quantile(values, 0.975))
